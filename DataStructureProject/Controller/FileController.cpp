ab28039848b4432d10280546a446d73437108ef2
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-01-31 12:19:53 -0700

    Created the FileController class and header files as well as a new Data folder with 3 files added into it.
    The data folder currently holds 3 files. 2 are part of the CrimeData class and header files and the 3rd is a spreadsheet that contains A LOT of data (thousands?) that the program will eventually read using the FileController.

---
+++ DataStructureProject/Controller/FileController.cpp
@@ -0,0 +1,9 @@
+//
+//  FileController.cpp
+//  DataStructureProject
+//
+//  Created by Singh, Kashish on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "FileController.hpp"
---
+++ DataStructureProject/Controller/FileController.hpp
@@ -0,0 +1,14 @@
+//
+//  FileController.hpp
+//  DataStructureProject
+//
+//  Created by Singh, Kashish on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef FileController_hpp
+#define FileController_hpp
+
+#include <stdio.h>
+
+#endif /* FileController_hpp */
