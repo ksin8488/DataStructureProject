75ea43038b8fb805bfd4650e029bab3300d25fb7
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-01-31 11:41:49 -0700

    Created the needed main files as well as some starting code for each.
    The main file was deleted and the runner now has the main.

---
+++ DataStructureProject/Controller/Controller.cpp
@@ -0,0 +1,16 @@
+//
+//  Controller.cpp
+//  DataStructureProject
+//
+//  Created by Singh, Kashish on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+using namespace std;
+
+void Controller :: start()
+{
+    
+}
---
+++ DataStructureProject/Runner.cpp
@@ -0,0 +1,15 @@
+//
+//  Runner.cpp
+//  DataStructureProject
+//
+//  Created by Singh, Kashish on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Controller/Controller.hpp"    //the #include leads to the filepath of the .hpp file
+int main()
+{
+    Controller app;
+    app.start();
+    return 0;
+}
