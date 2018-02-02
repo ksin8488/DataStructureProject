c3ea0968e50bc310349d9b259859a4b03cd918c0
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-02-02 12:37:31 -0700

    Coded the different methods for Array.hpp.
    The getSize() method just returns the size parameter since it is a getter and the getFromIndex() method is like the [] overloads while the setAtIndex() method asserts if the index is valid and then assigns it to the associated index in the internalArray.

--- DataStructureProject/Model/Structures/Array.hpp
+++ DataStructureProject/Model/Structures/Array.hpp
@@ -102,6 +102,31 @@ Type Array<Type> :: operator [] (int index) const
     return internalArray[index];
 }
     
+//Methods
+template <class Type>
+int Array<Type> :: getSize() const
+{
+    return size;
+}
+    
+template <class Type>
+Type Array<Type> :: getFromIndex(int index)
+{
+    assert(index >= 0 && index < size);
+    
+    Type value = internalArray[index];
+    
+    return value;
+}
+    
+template <class Type>
+void Array<Type> :: setAtIndex(int pos, Type item)
+{
+    assert(pos >= 0 && pos < size);
+    internalArray[pos] = item;
+}
+    
+    
 }
 
 #endif /* Array_h */
