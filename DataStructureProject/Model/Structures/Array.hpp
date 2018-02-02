251b558960f490c9824d57f1cb141cfecbb2f066
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-02-02 12:12:14 -0700

    Initialized the private and public data members and information for the class.
    Each method or constructor must have the line template <class Type> above its definition so the item becomes generic and will work with any Type.

--- DataStructureProject/Model/Structures/Array.hpp
+++ DataStructureProject/Model/Structures/Array.hpp
@@ -14,6 +14,30 @@
 
 using namespace std;    //Used for keyword access
 
-
+template <class Type>
+class Array
+{
+private:
+    Type * internalArray;
+    int size;
+public:
+    //Constructor
+    Array<Type>(int size);
+    
+    //Copy Constructor
+    Array<Type>(const Array<Type> & toCopy);
+    //Destructor
+    ~Array<Type>();
+    
+    //Operators
+    Array<Type> & operator = ()const Array<Type> & toReplace);
+    Type& operator [] (int index);
+    Type operator [] (int index) const;
+    
+    //Methods
+    int getSize() const;
+    Type get FromIndex(int index);
+    void setAtIndex(int index, Type data);
+}
 
 #endif /* Array_h */
