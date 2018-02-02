e974e4a74291e688d05255d7b96b1a83002378bd
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-02-02 12:23:04 -0700

    Created the constructors so they could set internalArray to a new array of a supplied size as well as a destructor.
    Also moved the runner back to the origional position due to errors appearing with trying to find the Controller file path.

--- DataStructureProject/Model/Structures/Array.hpp
+++ DataStructureProject/Model/Structures/Array.hpp
@@ -38,6 +38,36 @@ public:
     int getSize() const;
     Type get FromIndex(int index);
     void setAtIndex(int index, Type data);
+};
+
+template <class Type>
+Array<Type> :: Array(int size)
+{
+    assert(size> 0);
+    this -> size = size;
+    
+    internalArray = new Type[size];
+}
+
+template <class Type>
+Array<Type> :: Array(const Array<Type> & toCopy)
+{
+    this->size = toCopy.getSize();
+    
+    //Build Data Structure
+    internalArray = new Type[size];
+    
+    for(int index = 0; index < size; index++)
+    {
+        internalArray[index] = toCopy[index];
+    }
+    
+template <class Type>
+Array<Type> :: ~Array()
+{
+    delete [] internalArray;
+}
+    
 }
 
 #endif /* Array_h */
