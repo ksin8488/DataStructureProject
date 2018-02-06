190cb90aae8fd03752730725b8e39686aa13ab8b
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-02-06 12:12:18 -0700

    Created a new folder called "Testers" and created the ArrayTester class.
    It also contains the #include file directories for the Array.hpp, FileController.hpp, and Timer.hpp header files.
    
    NOTE: There is currently an error with the file directory for the Array.hpp file as of this commit.

---
+++ Testers/ArrayTester.hpp
@@ -0,0 +1,24 @@
+//
+//  ArrayTester.hpp
+//  DataStructureProject
+//
+//  Created by Singh, Kashish on 2/6/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef ArrayTester_hpp
+#define ArrayTester_hpp
+
+#include <iostream>
+#include "../Model/Structures/Array.hpp"    //Only 2 Dots! It is the command line sequence to the directory above
+#include "../Controller/Tools/FileController.hpp"
+#include "../Controller/Tools/Timer.hpp"
+
+class ArrayTester
+{
+public:
+    void testArrayUse();
+    void testAdvancedArrray();
+};
+
+#endif /* ArrayTester_hpp */
