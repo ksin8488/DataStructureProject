dd206fb591d3bb0b4c53e4859adbcb1e423cd838
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-02-08 12:55:56 -0700

    Created a new header file called List in the structures folder that creates a template class for list with the necessary data members, constructors, and methods.
    Also updated the NodeUML.uxf diagram file with new changes.

---
+++ DataStructureProject/Model/Structures/List.hpp
@@ -0,0 +1,33 @@
+//
+//  List.hpp
+//  DataStructureProject
+//
+//  Created by Singh, Kashish on 2/8/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef List_h
+#define List_h
+
+#include <assert.h>
+#include "../../Nodes/LinearNode.hpp"
+
+template <class Type>
+class List
+{
+public:
+    //Structure
+    virtual void add(Type item) = 0;
+    virtual void addAtIndex(int index, Type item) = 0;
+    virtual Type remove(int index) = 0;
+    virtual Type remove(int index) = 0;
+    //Helper
+    virtual int getSize() const = 0;
+    virtual LinearNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
+    
+protected:
+    int size;
+};
+
+#endif /* List_h */
