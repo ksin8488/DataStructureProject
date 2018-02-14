22a218c54d66478b6f3b439e091e099c5844ab2d
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-02-14 12:36:14 -0700

    Created a new tester class header.
    The header has two methods. One checks the functionality of a linked list and complies and operates it and the other loads and sees times with the file data.

---
+++ DataStructureProject/Testers/LinkedListTesters.hpp
@@ -0,0 +1,77 @@
+//
+//  LinkedListTesters.hpp
+//  DataStructureProject
+//
+//  Created by Singh, Kashish on 2/14/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef LinkedListTesters_h
+#define LinkedListTesters_h
+
+#include "../Model/Structures/Linear/LinkedList.hpp"
+#include "../Controller/FileController.hpp"
+#include "../Controller/Tools/Timer.hpp"
+#include "../Data/CrimeData.hpp"
+#include <iostream>
+
+class LinkedListTester
+{
+public:
+    void testListBasics();
+    void testListWithData();
+};
+
+void LinkedListTester :: testListBasics()
+{
+    LinkedList<int> numbers;
+    numbers.add(9);
+    cout << numbers.getFront()->getData() << " Is is at the front fo the list and shoudl be 9" << endl;
+    cout << numbers.getEnd()->getData() << " Is at teh end of the list and should be 9" << endl;
+    
+    numbers.add(0)
+    cout << numbers.getFront()->getData() << "Is is at the front of the list and should be 9" << endl;
+    cout << numbers.getEnbd()->getData() << " Is at the end of the list and should be 0" << endl;
+    
+    numbers.addAtIndex(1,2);
+    numbers.add(324);
+    
+    cout << numbers.getFront()->getData() << " Is is at the front fo the list and should be 9" << endl;
+    cout << numbers.getEnd()->getData() << " Is at the end of the llist and should be 324" << endl;
+    
+    cout << "THis loop should print 4 lines" << endl;
+    for(int index = 0; index < numbers.getSize(); index++)
+    {
+        cout << numbers.getFromIndex(index) << endl;
+    }
+    
+    numbers.remove(0);
+    numbers.add(32567);
+    numbers.addAtIndex(0,2312);
+    
+    cout << numbers.getFront()->getData() << " Is is at teh front of the list and hsould be 2312" << endl;
+    cout  << numbers.getEnd()->getData() << " IS at the end of the list and should be 32567" << endl;
+}
+
+void LinkedListTEster :: testListWithData()
+{
+    Timer listTimer;
+    
+    listTimer.startTimer();
+    LinkedLKist<CrimesData> crimes = FileController :: readDataToList("/Users/ksin8488/SwiftProjectsPM/DataStructureProject/DataStructureProject/Data/crime.csv");
+    listTimer.stopTimer();
+    cout << "This is how long it took to read the structure into our custom data structure" << endl;
+    listTimer.displayInformation();
+    
+    listTimer.resetTimer();
+    cout << "Here is how long it tkaes to access a random data value" << endl;
+    listTimer.startTImer();
+    int randomLocation = (rand() * rand()) % crimes.getSize();
+    cout << "THe random index is " << randomLocation << endl;
+    double totalViolentRate = crimes.getFromIndex(randomLocation).getAllViolentRates();
+    listTimer.stopTimer();
+    cout << "THe random crime stat is: " << totalViolentRate << " , and here is the time" << endl;
+    listTimer.displayInformation();
+}
+
+#endif /* LinkedListTesters_h */
