e562081aa1a3604d67767724bb5680520ed2bcdf
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-03-27 12:37:47 -0600

    Created the BinaryTreeNode header file in the Nodes file. And added a UML diagram for it.

---
+++ DataStructureProject/Model/Nodes/BinaryTreeNode.hpp
@@ -0,0 +1,71 @@
+//
+//  BinaryTreeNode.hpp
+//  DataStructureProject
+//
+//  Created by Singh, Kashish on 3/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeNode_h
+#define BinaryTreeNode_h
+
+#include "Nodes.hpp"
+
+template <class Type>
+class BinaryTreeNode : public Node<Type>        //going to be used to sort the CrimeData
+{
+private:
+    BinaryTreeNode<Type> * root;
+    BinaryTreeNode<Type> * left;
+    BinaryTreeNode<Type> * right;
+    
+public:
+    BinaryTreeNode();
+    BinaryTreeNode(Type data);
+    
+    BinaryTreeNode<Type> * getRootNode();
+    BinaryTreeNode<Type> * getLeftNode();
+    BinaryTreeNode<Type> * getRightNode();
+    
+    void setRootNode(BinaryTreeNode<Type> *);
+    void setLeftNode(BinaryTreeNode<Type> *);
+    void setRightNode(BinaryTreeNode<Type> *);
+};
+
+template <class Type>
+BinaryTreeMode<Type> :: BinaryTreeNode() : Node<Type()
+{
+    root = nullptr;
+    left = nullptr;
+    right = nullptr;
+}
+
+template <class Type>
+BinaryTreeNode<Type> :: BinaryTreeNode(Type data) : Node<Type>(data)
+{
+    root = nullptr;
+    left = nullptr;
+    right = nullptr;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRootNode()
+{
+    return this->root;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRightNode()
+{
+    return this->right;
+}
+
+template <class type>
+BinaryTreeNode<Type> * BinartyTreeNode<Type> :: getLeftNode()
+{
+    return this->left;
+}
+
+
+
+#endif /* BinaryTreeNode_h */
