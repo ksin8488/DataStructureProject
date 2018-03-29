9fd354c2c4baa604e8a95d6353a624341ce90c56
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-03-29 12:05:04 -0600

    Coded the class header in the Tree.hpp and newly made BinarySearchTree.hpp files..
    Also the BinaryTreeUML.uxf file has been updated to now contain a UML for BinarySearchTree.

---
+++ DataStructureProject/Model/Structures/NonLinear/BinarySearchTree.hpp
@@ -0,0 +1,63 @@
+//
+//  BinarySearchTree.hpp
+//  DataStructureProject
+//
+//  Created by Singh, Kashish on 3/29/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef BinarySearchTree_h
+#define BinarySearchTree_h
+
+#include "Tree.hpp"
+#include <iostream>
+using namespace std;
+
+template <class Type>
+class BinarySearchTree : public Tree<Type>
+{
+protected:
+    int calculateSize(BinaryTreeNode<Type> * startNode);
+    int calculateHeight(BinaryTreeNode<Type> * startNode);
+    bool isBalenced(BinaryTreeNode<Type> * startNode);
+    bool isComplete(BinaryTreeNode<Type> * startNode, int index, int size);
+    
+    void inOrderTraversal(BinaryTreeNode<Type> * inStart);
+    void preOrderTraversal(BinaryTreeNode<Type> * preStart);
+    void postOrderTraversal(BinaryTreeNode<Type> * postStart);
+    
+    BinaryTreeNode<Type> * getRightMostChild(BinaryTreeNode<Type> * current);
+    BinaryTreeNode<Type> * getLeftMostChild(BinaryTreeNode<Type> * current);
+    
+    void removeNode(BinaryTreeNode<Type> * removeMe);
+    
+public:
+    BinarySearchTree();
+    ~BinarySearchTree();
+    
+    BinaryTreeNode<Type> * getRoot();
+    void setRoot(BinaryTreeNode<Type> * root);
+    
+    void inOrderTraversal();
+    void preOrderTraversal();
+    void postOrderTraversal();
+    
+    int getSize();
+    int getHeight();
+    bool isComplete();
+    bool isBalanced();
+    
+    bool contains(Type value);
+    void insert(Type itemToInsert);
+    void remove(Type value);
+    
+    Type findMinimum();
+    Type findMaximum();
+};
+
+template class<Type>
+
+
+
+
+#endif /* BinarySearchTree_h */
---
+++ DataStructureProject/Model/Structures/NonLinear/Tree.hpp
@@ -0,0 +1,34 @@
+//
+//  Tree.hpp
+//  DataStructureProject
+//
+//  Created by Singh, Kashish on 3/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Tree_h
+#define Tree_h
+
+#include "../../Nodes/BinaryTreeNode.hpp"
+template <class Type>
+class Tree
+{
+protected:
+    BinaryTreeNode<Type> * root;
+public:
+    //Informational Methods
+    virtual int getSize() = 0;  //virtual & = 0 makes it abstract
+    virtual int getHeight() = 0;
+    virtual bool isCompleted() = 0;
+    virtual bool isBalenced() = 0;
+    //*Data Methods*
+    virtual void insert(Type data) = 0;
+    virtual bool contains(Type data) = 0;
+    virtual void remove(Type data) = 0;
+    //*Traversals*
+    virtual void inOrderTraversal() = 0;
+    virtual void preOrderTraversal() = 0;
+    virtual void postOrderTraversal() = 0;
+};
+
+#endif /* Tree_h */
